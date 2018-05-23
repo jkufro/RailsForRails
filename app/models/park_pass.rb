7f510bbaf3b4e08cd7edd249a3fa298b9fa3d4d5
Justin Kufro <jkufro@andrew.cmu.edu>
2018-05-23 23:01:51 +0000

    passed all basic model tests

--- app/models/park_pass.rb
+++ app/models/park_pass.rb
@@ -2,4 +2,5 @@ class ParkPass < ApplicationRecord
     belongs_to :pass_type
     belongs_to :user 
     has_many :visits
+    has_many :quueues, through: :visits
 end
--- test/models/quueue_test.rb
+++ test/models/quueue_test.rb
@@ -3,5 +3,4 @@ require 'test_helper'
 class QuueueTest < ActiveSupport::TestCase
   should belong_to(:ride)
   should belong_to(:visit)
-  should belong_to(:park_pass).through(:visit)
 end
