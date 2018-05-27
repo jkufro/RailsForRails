97ffcf9c865dc48d7bda993ce05069588f336902
Justin Kufro <jkufro@andrew.cmu.edu>
2018-05-27 15:11:25 +0000

    tests for queue codes

--- app/models/quueue.rb
+++ app/models/quueue.rb
@@ -1,4 +1,15 @@
 class Quueue < ApplicationRecord
     belongs_to :visit
     belongs_to :ride
+    
+    should allow_value("AAAAD").for(:queue_code)
+    should allow_value("AGDES").for(:queue_code)
+    should allow_value("ZZZZZ").for(:queue_code)
+    should allow_value("IIIII").for(:queue_code)
+    should_not allow_value("ASDC").for(:queue_code)
+    should_not allow_value("AFDSAD").for(:queue_code)
+    should_not allow_value("AAAAa").for(:queue_code)
+    should_not allow_value("aFSDW").for(:queue_code)
+    should_not allow_value("FFF4W").for(:queue_code)
+    should_not allow_value("11111").for(:queue_code)
 end
