41f35cdfb6bf10839b106e2a5ddcde6ef7baefff
Justin Kufro <jkufro@andrew.cmu.edu>
2018-05-27 15:04:13 +0000

    tests for park pass number

--- test/models/park_pass_test.rb
+++ test/models/park_pass_test.rb
@@ -5,4 +5,13 @@ class ParkPassTest < ActiveSupport::TestCase
   should belong_to(:user)
   should have_many(:visits)
   should have_many(:quueues).through(:visits)
+  
+  should allow_value("V761997609331450").for(:card_number)
+  should allow_value("P657816359167903").for(:card_number)
+  should allow_value("K100983943647243").for(:card_number)
+  should allow_value("P969196322550527").for(:card_number)
+  should_not allow_value("4969196322550527").for(:card_number)
+  should_not allow_value("P96919632255052A").for(:card_number)
+  should_not allow_value("P9691963225505274").for(:card_number)
+  should_not allow_value("P96919632255527").for(:card_number)
 end
