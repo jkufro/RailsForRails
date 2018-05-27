2c43b3b9a0b8e2ba846bc86d8e8f6b04db6b1cca
Justin Kufro <jkufro@andrew.cmu.edu>
2018-05-27 15:07:19 +0000

    passed card format tests

--- app/models/park_pass.rb
+++ app/models/park_pass.rb
@@ -3,4 +3,7 @@ class ParkPass < ApplicationRecord
     belongs_to :user 
     has_many :visits
     has_many :quueues, through: :visits
+    
+    
+    validates_format_of :card_number, with: /\A[A-Z]\d{15}\z/, message: "is not a valid format"
 end
