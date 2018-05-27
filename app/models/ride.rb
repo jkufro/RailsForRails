177929753bf98853c8ad3ff9a7a92799e16b91ae
Justin Kufro <jkufro@andrew.cmu.edu>
2018-05-27 15:14:37 +0000

    queue code validation for rides too

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,5 @@
 class Ride < ApplicationRecord
     has_many :quueues
+    
+    validates_format_of :max_allowed_queue_code, with: /\A[A-Z]{5}\z/, message: "must be five upper case letters"
 end
--- test/models/ride_test.rb
+++ test/models/ride_test.rb
@@ -2,4 +2,15 @@ require 'test_helper'
 
 class RideTest < ActiveSupport::TestCase
   should have_many(:quueues)
+  
+  should allow_value("AAAAD").for(:max_allowed_queue_code)
+  should allow_value("AGDES").for(:max_allowed_queue_code)
+  should allow_value("ZZZZZ").for(:max_allowed_queue_code)
+  should allow_value("IIIII").for(:max_allowed_queue_code)
+  should_not allow_value("ASDC").for(:max_allowed_queue_code)
+  should_not allow_value("AFDSAD").for(:max_allowed_queue_code)
+  should_not allow_value("AAAAa").for(:max_allowed_queue_code)
+  should_not allow_value("aFSDW").for(:max_allowed_queue_code)
+  should_not allow_value("FFF4W").for(:max_allowed_queue_code)
+  should_not allow_value("11111").for(:max_allowed_queue_code)
 end
