aaa5d34acb3eb7d85f17ec6aa77513ea2c61f914
Justin Kufro <jkufro@andrew.cmu.edu>
2018-05-27 15:21:03 +0000

    validate positive nonzero integer for carts_on_track

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -2,4 +2,5 @@ class Ride < ApplicationRecord
     has_many :quueues
     
     validates_format_of :max_allowed_queue_code, with: /\A[A-Z]{5}\z/, message: "must be five upper case letters"
+    validates_numericality_of :carts_on_track, :greater_than => 0, :only_integer => true
 end
--- test/models/ride_test.rb
+++ test/models/ride_test.rb
@@ -13,4 +13,16 @@ class RideTest < ActiveSupport::TestCase
   should_not allow_value("aFSDW").for(:max_allowed_queue_code)
   should_not allow_value("FFF4W").for(:max_allowed_queue_code)
   should_not allow_value("11111").for(:max_allowed_queue_code)
+  
+  should validate_numericality_of(:carts_on_track) 
+  should allow_value(1).for(:carts_on_track)
+  should allow_value(100).for(:carts_on_track)
+  should allow_value(60).for(:carts_on_track)
+  should allow_value(22.0).for(:carts_on_track)
+  should_not allow_value(0.1).for(:carts_on_track)
+  should_not allow_value(-0.1).for(:carts_on_track)
+  should_not allow_value(-1).for(:carts_on_track)
+  should_not allow_value(20.0000001).for(:carts_on_track)
+  
+  
 end
