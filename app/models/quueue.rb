0631c18a040376023c8d49f70b09c50949a51218
Justin Kufro <jkufro@andrew.cmu.edu>
2018-05-27 15:13:28 +0000

    passed queue code tests

--- app/models/quueue.rb
+++ app/models/quueue.rb
@@ -1,15 +1,6 @@
 class Quueue < ApplicationRecord
-    belongs_to :visit
-    belongs_to :ride
-    
-    should allow_value("AAAAD").for(:queue_code)
-    should allow_value("AGDES").for(:queue_code)
-    should allow_value("ZZZZZ").for(:queue_code)
-    should allow_value("IIIII").for(:queue_code)
-    should_not allow_value("ASDC").for(:queue_code)
-    should_not allow_value("AFDSAD").for(:queue_code)
-    should_not allow_value("AAAAa").for(:queue_code)
-    should_not allow_value("aFSDW").for(:queue_code)
-    should_not allow_value("FFF4W").for(:queue_code)
-    should_not allow_value("11111").for(:queue_code)
+  belongs_to :visit
+  belongs_to :ride
+  
+  validates_format_of :queue_code, with: /\A[A-Z]{5}\z/, message: "must be five upper case letters"
 end
--- test/models/quueue_test.rb
+++ test/models/quueue_test.rb
@@ -3,4 +3,15 @@ require 'test_helper'
 class QuueueTest < ActiveSupport::TestCase
   should belong_to(:ride)
   should belong_to(:visit)
+  
+  should allow_value("AAAAD").for(:queue_code)
+  should allow_value("AGDES").for(:queue_code)
+  should allow_value("ZZZZZ").for(:queue_code)
+  should allow_value("IIIII").for(:queue_code)
+  should_not allow_value("ASDC").for(:queue_code)
+  should_not allow_value("AFDSAD").for(:queue_code)
+  should_not allow_value("AAAAa").for(:queue_code)
+  should_not allow_value("aFSDW").for(:queue_code)
+  should_not allow_value("FFF4W").for(:queue_code)
+  should_not allow_value("11111").for(:queue_code)
 end
