d846c820b5bc94b216cc72e2fc0e4e12d04161f0
Justin Kufro <kufroj@gmail.com>
2018-07-02 12:04:15 -0400

    use materialize suggested header, main, footer structure and added sticky footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,35 @@
 //= require materialize-form
 //= require vue
 //= require_tree .
+
+
+$( document ).ready(function() {
+    var footerHeight = 0,
+           footerTop = 0,
+           $footer = $("#footer");
+
+       positionFooter();
+
+       function positionFooter() {
+
+                footerHeight = $footer.height();
+                footerTop = ($(window).scrollTop()+$(window).height()-footerHeight)+"px";
+
+               if ( ($(document.body).height()+footerHeight) < $(window).height()) {
+                   $footer.css({
+                        position: "absolute"
+                   }).animate({
+                        top: footerTop
+                   })
+               } else {
+                   $footer.css({
+                        position: "static"
+                   })
+               }
+
+       }
+
+       $(window)
+               .scroll(positionFooter)
+               .resize(positionFooter)
+});
